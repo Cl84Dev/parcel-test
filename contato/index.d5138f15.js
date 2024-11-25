f14b2da42f02623e5b92ebccdd516bfbbec6428e
Claudio <cfahifi@hotmail.com>
2024-11-25 18:16:54 -0300

    feat: parcel test

---
+++ contato/index.d5138f15.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".hamburger-menu"),t=document.querySelector("#btn-menu");t&&(t.addEventListener("click",()=>{e.classList.toggle("active")}),e.addEventListener("click",()=>{e.classList.remove("active")}));
+//# sourceMappingURL=index.d5138f15.js.map
---
+++ index.607441c7.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".hamburger-menu"),t=document.querySelector("#btn-menu");t&&(t.addEventListener("click",()=>{e.classList.toggle("active")}),e.addEventListener("click",()=>{e.classList.remove("active")}));
+//# sourceMappingURL=index.607441c7.js.map
